e62930b6538b4082053e596c8bd0bde3a9bb3ad3
Nicolò Guerra <guerragianni0503@gmail.com>
2021-11-17 19:36:55 +0100

    Implemented SimpleGUI

--- src/it/unibo/oop/lab/mvc/SimpleGUI.java
+++ src/it/unibo/oop/lab/mvc/SimpleGUI.java
@@ -1,9 +1,15 @@
 package it.unibo.oop.lab.mvc;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  * A very simple program using a graphical interface.
@@ -20,15 +26,15 @@ public final class SimpleGUI {
      * 
      * 2) In its constructor, sets up the whole view
      * 
-     * 3) The graphical interface consists of a JTextField in the upper part of the frame, 
-     * a JTextArea in the center and two buttons below it: "Print", and "Show history". 
-     * SUGGESTION: Use a JPanel with BorderLayout
+     * 3) The graphical interface consists of a JTextField in the upper part of the
+     * frame, a JTextArea in the center and two buttons below it: "Print", and
+     * "Show history". SUGGESTION: Use a JPanel with BorderLayout
      * 
      * 4) By default, if the graphical interface is closed the program must exit
      * (call setDefaultCloseOperation)
      * 
-     * 5) The behavior of the program is that, if "Print" is pressed, the
-     * controller is asked to show the string contained in the text field on standard output. 
+     * 5) The behavior of the program is that, if "Print" is pressed, the controller
+     * is asked to show the string contained in the text field on standard output.
      * If "show history" is pressed instead, the GUI must show all the prints that
      * have been done to this moment in the text area.
      * 
@@ -38,16 +44,17 @@ public final class SimpleGUI {
      * builds a new {@link SimpleGUI}.
      */
     public SimpleGUI() {
-
+        Controller cont = new ControllerImpl();
+        
         /*
-         * Make the frame half the resolution of the screen. This very method is
-         * enough for a single screen setup. In case of multiple monitors, the
-         * primary is selected.
+         * Make the frame half the resolution of the screen. This very method is enough
+         * for a single screen setup. In case of multiple monitors, the primary is
+         * selected.
          * 
-         * In order to deal coherently with multimonitor setups, other
-         * facilities exist (see the Java documentation about this issue). It is
-         * MUCH better than manually specify the size of a window in pixel: it
-         * takes into account the current resolution.
+         * In order to deal coherently with multimonitor setups, other facilities exist
+         * (see the Java documentation about this issue). It is MUCH better than
+         * manually specify the size of a window in pixel: it takes into account the
+         * current resolution.
          */
         final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         final int sw = (int) screen.getWidth();
@@ -55,11 +62,38 @@ public final class SimpleGUI {
         frame.setSize(sw / 2, sh / 2);
 
         /*
-         * Instead of appearing at (0,0), upper left corner of the screen, this
-         * flag makes the OS window manager take care of the default positioning
-         * on screen. Results may vary, but it is generally the best choice.
+         * Instead of appearing at (0,0), upper left corner of the screen, this flag
+         * makes the OS window manager take care of the default positioning on screen.
+         * Results may vary, but it is generally the best choice.
          */
         frame.setLocationByPlatform(true);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        final JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        frame.add(mainPanel);
+
+        final JTextField input = new JTextField();
+        mainPanel.add(input, BorderLayout.NORTH);
+
+        final JTextArea history = new JTextArea();
+        mainPanel.add(history, BorderLayout.CENTER);
+
+        final JPanel bottomButtons = new JPanel();
+        bottomButtons.setLayout(new BoxLayout(bottomButtons, BoxLayout.X_AXIS));
+        final JButton print = new JButton("Print");
+        final JButton showHistory = new JButton("Show history");
+        bottomButtons.add(print);
+        bottomButtons.add(showHistory);
+
+        mainPanel.add(bottomButtons, BorderLayout.SOUTH);
+
+        frame.setVisible(true);
+    }
+
+    public static void main(final String[] args) {
+        new SimpleGUI();
     }
 
 }
