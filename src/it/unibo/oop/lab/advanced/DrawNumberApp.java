09928150f35b11b0f14f2830034f3029e060a0f1
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2021-11-18 11:55:35 +0100

    Modified DrawNumberApp to load config from yaml file

--- src/it/unibo/oop/lab/advanced/DrawNumberApp.java
+++ src/it/unibo/oop/lab/advanced/DrawNumberApp.java
@@ -1,21 +1,62 @@
 package it.unibo.oop.lab.advanced;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.stream.Stream;
+
 /**
  */
 public final class DrawNumberApp implements DrawNumberViewObserver {
 
-    private static final int MIN = 0;
-    private static final int MAX = 100;
-    private static final int ATTEMPTS = 10;
+    private static final String FILE_NAME = "res" + System.getProperty("file.separator") + "config.yml";
+    private static final String SEPARATOR_CHAR = ":";
+    private static final String YAML_MINIMUM = "minimum";
+    private static final String YAML_MAXIMUM = "maximum";
+    private static final String YAML_ATTEMPTS = "attempts";
     private final DrawNumber model;
     private final DrawNumberView view;
+    private int min;
+    private int max;
+    private int attempts;
+
+    private void loadFromFile() throws IOException {
+        /* Load from file */
+        try (BufferedReader r = new BufferedReader(new FileReader(FILE_NAME))) {
+            final Iterator<String> linesIterator = r.lines().iterator();
+            while (linesIterator.hasNext()) {
+                final String[] param = linesIterator.next().split(SEPARATOR_CHAR);
+
+                switch (param[0].trim()) {
+                case YAML_MINIMUM:
+                    min = Integer.parseInt(param[1].trim());
+                    break;
+                case YAML_MAXIMUM:
+                    max = Integer.parseInt(param[1].trim());
+                    break;
+                case YAML_ATTEMPTS:
+                    attempts = Integer.parseInt(param[1].trim());
+                    break;
+                default:
+                    throw new IOException("Wrong file format");
+                }
+            }
+        }
+    }
 
     /**
      * 
      */
     public DrawNumberApp() {
-        this.model = new DrawNumberImpl(MIN, MAX, ATTEMPTS);
         this.view = new DrawNumberViewImpl();
+        try {
+            loadFromFile();
+        } catch (IOException e) {
+            view.displayError(e.getMessage());
+        }
+        this.model = new DrawNumberImpl(min, max, attempts);
         this.view.setObserver(this);
         this.view.start();
     }
@@ -44,7 +85,7 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
 
     /**
      * @param args
-     *            ignored
+     *                 ignored
      */
     public static void main(final String... args) {
         new DrawNumberApp();
